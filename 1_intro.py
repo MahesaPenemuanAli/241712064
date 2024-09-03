bedb2f9af89eae3efdf746f23b57cf4d87966534
MahesaPenemuanAli <mahesaali098@gmail.com>
2024-09-03 12:35:42 +0700

    perkuliahan 1

---
+++ 1_intro.py
@@ -0,0 +1,25 @@
+# Deklarasi Variabel
+a = float(10.5)
+b = 5
+c = a+b
+
+print(a)
+print(b)
+print(c)
+
+# Operasi (operatio) -> tambah(+)
+# , kurang (-)
+# , bagi(/), kali(*), modulo (%), pankat(**)
+
+pekalian = a * b
+pembagian = a / b
+pengurangan = a - b
+modulo = a % b
+pangkat = a ** b
+
+print(pekalian)
+print(pembagian)
+print(pengurangan)
+print(modulo)
+print(pangkat)
+
